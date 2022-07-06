80874dfb9da8cdfb45430a21b78f6e289688ea86
MauricioDMO <mauriciodmoprogramer@gmail.com>
2022-07-06 11:11:20 -0600

    No se lo que paso con git

--- pokeload.py
+++ pokeload.py
@@ -1,7 +1,6 @@
 import pickle
 from requests_html import HTMLSession
 from os import system
-
 pokemon_base = {
     'name': "",
     'current_health': 100,
@@ -11,43 +10,29 @@ pokemon_base = {
     'attacks': None,
     'current_exp': 0
 }
-
 elements = ['acero', 'agua', 'bicho', 'dragon', 'electrico', 'fantasma', 'fuego', 'hada', 'hielo',
     'lucha', 'normal', 'planta', 'psiquico', 'roca', 'siniestro', 'tierra', 'veneno', 'volador']
-
 URL_BASE = 'https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/movimientos_nivel&pk='
 
 def find_elements(pokemon_page):
-<<<<<<< HEAD
 
     pokemon_elements = []
-    
+
     for element in elements:
-    
+
         if pokemon_page.html.find('td[class=bordeambos]:not(td[align="center"]) img[alt={}]'.format(element)):
-    
+
             pokemon_elements.append(element)
-    
-=======
-    pokemon_elements = []
-    for element in elements:
-        if pokemon_page.html.find('td[class=bordeambos]:not(td[align="center"]) img[alt={}]'.format(element)):
-            pokemon_elements.append(element)
->>>>>>> 7864afc919c0d018195aab45cda171761b237967
+
     return pokemon_elements
 
 
 def find_attacks(pokepage):
-<<<<<<< HEAD
-    
+
     attacks = []
-    
+
     for attack_item in pokepage.html.find(".pkmain")[-1].find("tr .check3"):
-    
-=======
-    attacks = []
-    for attack_item in pokepage.html.find(".pkmain")[-1].find("tr .check3"):
->>>>>>> 7864afc919c0d018195aab45cda171761b237967
+
         attack = {
             'name': attack_item.find('td', first=True).find('a', first=True).text,
             'type': attack_item.find("td")[1].find("img", first=True).attrs["alt"],
@@ -60,27 +45,18 @@ def find_attacks(pokepage):
 
 
 def get_pokemon(index):
-<<<<<<< HEAD
-    
-=======
->>>>>>> 7864afc919c0d018195aab45cda171761b237967
+
     url = '{}{}'.format(URL_BASE, index)
     session = HTMLSession()
     pokemon_page = session.get(url)
-
     pokemon_name = pokemon_page.html.find('.mini', first=True).text.split('\n')[0]
-
     pokemon_types = find_elements(pokemon_page)
-
     attacks = find_attacks(pokemon_page)
-
     pokemon_info = pokemon_base.copy()
     pokemon_info['name'] = pokemon_name
     pokemon_info['type'] = pokemon_types
     pokemon_info['attacks'] = attacks
-
     return pokemon_info
-
 def load(i):
     i += 1
     i /= 151
@@ -90,8 +66,6 @@ def load(i):
     load = '[{}{}] {} %'.format('#' * i, '-' * (10 - i), percent)
     system('cls')
     print(load)
-
-
 def get_all_pokemons():
     
     try:
@@ -116,30 +90,20 @@ def get_all_pokemons():
             pickle.dump(all_pokemons, pokedex)
         
         print('Todos los pokemons han sido descargados!')
-    
+
     print('Lista de pokemons cargada!')
-<<<<<<< HEAD
-    
+
     return all_pokemons
 
 def main():
-    
-    lista = []
-    
-=======
-    return all_pokemons
 
-def main():
     lista = []
->>>>>>> 7864afc919c0d018195aab45cda171761b237967
+
     for i in get_all_pokemons():
         for a in i['attacks']:
             if not a['type'] in lista:
                 lista.append(a['type'])
-<<<<<<< HEAD
-    
-=======
->>>>>>> 7864afc919c0d018195aab45cda171761b237967
+
     print(lista)
 
 
